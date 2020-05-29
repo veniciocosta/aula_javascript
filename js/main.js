f430a5b6ab24391d5b6777148bd71c7aa0ef6b44
Venicio Assunção da Costa <veniciosss@gmail.com>
2020-05-29 16:00:32 -0300

    material final da aula

--- js/main.js
+++ js/main.js
@@ -7,14 +7,18 @@ alert("Agora vamos substituir seu sobre nome e deixar tudo miúsculo")
 alert(nome.toUpperCase() + " " + sobrenome.replace(sobrenome,"Mudei Seu Sobrenome").toUpperCase())
 */
 
+
+/*
+function minhaSoma(num1,num2){
+    return num1 + num2
+}
+
+alert(minhaSoma(10,5))
+*/
+
 function primeira_acao()
 {
     alert("Você clicou")
-}
-
-function passou_mouse(elemento)
-{
-    elemento    
 }
 
 /*
@@ -54,18 +58,71 @@ else
 }
 */
 
-
+/*
 function teste_idade(idade){
-    var resposta
-    idade=prompt("Qual sua idade?")
 
     if (idade>=18)
     {
-        alert("maior de idade")
+        return "maior de idade"
     }
     else
     {
-        alert("menor de idade")
+        return "menor de idade"
     }
 
+}
+*/
+
+//alert(teste_idade(prompt("Qual sua idades?")))
+
+
+function extrairData(elemento)
+{
+    //var datacompleta =  new Date()
+    //var resposta
+    
+    /*
+    if (elemento.value = "1")
+    {
+        resposta = datacompleta.getDate()
+    }
+    
+    if (elemento.value = "2")
+    {
+        resposta = datacompleta.getMonth()+1
+    }
+    if (elemento.value = "3")
+    {
+        resposta = datacompleta.getFullYear()
+    }
+    if (elemento.value = "4")
+    {
+        resposta = datacompleta.getDay()
+    }
+    */
+    document.getElementById("dataEscolha").innerText = elemento.value
+    
+    //document.getElementById("dataEscolha").innerText=
+}
+
+function elementoID() {
+    alert(document.getElementById("btnTeste"))
+    alert("Repare que o nome do botão irá mudar")
+    document.getElementById("btnTeste").innerText = "Mudou"
+    //alert("Repare que o nome do botão irá retornar")
+    //document.getElementById("btnTeste").innerText = "Clique aqui"
+}
+
+
+function passou_mouse(elemento){
+    elemento.innerHTML = "<b> Passou o mouse </b>"
+}
+
+function tirou_mouse(elemento){
+    elemento.innerHTML = "Passe o mouse aqui"
+    document.getElementById("textodois").innerText="Passe o mouse aqui também"
+}
+
+function outroSite(){
+    window.open("https://www.google.com")
 }
